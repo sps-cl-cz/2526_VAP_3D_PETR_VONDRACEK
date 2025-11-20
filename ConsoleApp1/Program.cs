35db172494b755a24df9cff0d92395c02d593473
3.D Vondráček Petr <Petr.Vondracek@student.sps-cl.cz>
2025-11-20 16:24:16 +0100

    zadání 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿//hodnotové datové typy
+double a = 0;
+double b = 0;
+double vysledek = 0;
+
+//boolovská proměnná pro kontrolu platnosti operátoru
+bool platnyOper = true;
+
+//zadání vstupů od uživatele
+Console.WriteLine("Zadej první číslo:");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Zadej operátor (+, -, *, /):");
+string op = Console.ReadLine();
+
+Console.WriteLine("Zadej druhé číslo:");
+b = Convert.ToDouble(Console.ReadLine());
+
+//výpočet podle zvoleného operátoru
+if (op == "+")
+{
+    vysledek = a + b;
+}
+else if (op == "-")
+{
+    vysledek = a - b;
+}
+else if (op == "*")
+{
+    vysledek = a * b;
+}
+else if (op == "/")
+{
+    if (b == 0)
+    {
+        Console.WriteLine("Chyba: nelze dělit nulou!");
+        platnyOper = false;
+    }
+    else
+    {
+        vysledek = a / b;
+    }
+}
+else
+{
+    Console.WriteLine("Chyba: neznámý operátor!");
+    platnyOper = false;
+}
+
+//výpis výsledku
+if (platnyOper)
+{
+    Console.WriteLine("Výsledek: " + vysledek);
+}
+
+Console.WriteLine("\nStiskni libovolnou klávesu pro ukončení...");
+Console.ReadKey();
