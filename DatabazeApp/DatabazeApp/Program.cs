6a8a2e6b23faf23eb196e511175283f073dfffa6
3.D Vondráček Petr <Petr.Vondracek@student.sps-cl.cz>
2026-01-19 11:41:44 +0100

    prace s databazemi

---
+++ DatabazeApp/DatabazeApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.Sqlite;
+using SQLiteApp;
+
+namespace Daatabase;
+
+class Proram
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Zadejte sve jmeno.");
+            return;
+        }
+        Game game = new Game();
+        int score = game.Play();
+        string connectionString = "Data source=app.db";
+        using SqliteConnection connection = new SqliteConnection(connectionString);
+        connection.Open();
+        using SqliteCommand createTableCommand = connection.CreateCommand();
+        createTableCommand.CommandText = @"
+                CREATE TABLE IF NOT EXISTS Players(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                       Srore INTEGER MOT NULL
+                );
+            ";
+        createTableCommand.ExecuteNonQuery();
+
+        using SqliteCommand readCommand = connection.CreateCommand();
+        readCommand.CommandText = "SELECT FROM Score Players WHERE Name = $Name;";
+        readCommand.Parameters.AddWithValue("$Name", args[0]);
+        object result = readCommand.ExecuteScalar();
+        if (result == null) {;
+
+    };
+
+        using SqliteCommand insertCommand = connection.CreateCommand();
+        insertCommand.CommandText = "INSERT INTO Players(Name, Score) Values($Name, $Score);";
+        insertCommand.Parameters.AddWithValue("$Name", args[0]);
+        insertCommand.Parameters.AddWithValue("$Score", score);
+        insertCommand.ExecuteNonQuery();
+
+        } else
+        {
+            long highScore = (long)result;
+
+        }
+    }
+}
+
+
